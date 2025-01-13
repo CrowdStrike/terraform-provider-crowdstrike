0c1c46cf9dc9d8d43b4cb7b4e18b923ae0f2ad27
Alexandru Gandrabur <alexandru.gandrabur@crowdstrike.com>
2025-01-13 17:59:51 +0200

    add ioa and dspm fields to cspm aws account

--- internal/fcs/cspm_aws_account.go
+++ internal/fcs/cspm_aws_account.go
@@ -9,12 +9,14 @@ import (
 	"github.com/crowdstrike/gofalcon/falcon/client/cspm_registration"
 	"github.com/crowdstrike/gofalcon/falcon/models"
 	"github.com/crowdstrike/terraform-provider-crowdstrike/internal/scopes"
+	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -29,12 +31,19 @@ type cspmAWSAccountResource struct {
 type cspmAWSAccountModel struct {
 	AccountID                types.String `tfsdk:"account_id"`
 	OrganizationID           types.String `tfsdk:"organization_id"`
+	TargetOUs                types.List   `tfsdk:"target_ous"`
 	EnableRealtimeVisibility types.Bool   `tfsdk:"enable_realtime_visibility"`
 	EnableSensorManagement   types.Bool   `tfsdk:"enable_sensor_management"`
 	EnableDSPM               types.Bool   `tfsdk:"enable_dspm"`
 	ExternalID               types.String `tfsdk:"external_id"`
 	IntermediateRoleArn      types.String `tfsdk:"intermediate_role_arn"`
 	IamRoleArn               types.String `tfsdk:"iam_role_arn"`
+	EventbusName             types.String `tfsdk:"eventbus_name"`
+	EventbusArn              types.String `tfsdk:"eventbus_arn"`
+	CloudTrailBucketName     types.String `tfsdk:"cloudtrail_bucket_name"`
+	CloudTrailRegion         types.String `tfsdk:"cloudtrail_region"`
+	DSPMRoleName             types.String `tfsdk:"dspm_role_name"`
+	DSPMRoleArn              types.String `tfsdk:"dspm_role_arn"`
 }
 
 // Ensure the implementation satisfies the expected interfaces.
@@ -95,6 +104,20 @@ func (r *cspmAWSAccountResource) Schema(
 					stringvalidator.RegexMatches(regexp.MustCompile(`^o-[a-z0-9]{10,32}$`), "must be in the format of o-xxxxxxxxxx"),
 				},
 			},
+			"target_ous": schema.ListAttribute{
+				Optional:    true,
+				Description: "The list of target OUs",
+				PlanModifiers: []planmodifier.List{
+					listplanmodifier.UseStateForUnknown(),
+				},
+				ElementType: types.StringType,
+				Validators: []validator.List{
+					listvalidator.ValueStringsAre(
+						stringvalidator.LengthBetween(16, 68),
+						stringvalidator.RegexMatches(regexp.MustCompile(`^ou-[0-9a-z]{4,32}-[a-z0-9]{8,32}$`), "must be in the format of ou-xxxx-xxxxxxxx"),
+					),
+				},
+			},
 			"enable_realtime_visibility": schema.BoolAttribute{
 				Optional:    true,
 				Default:     booldefault.StaticBool(false),
@@ -131,6 +154,50 @@ func (r *cspmAWSAccountResource) Schema(
 				Computed: true,
 				// Optional: true, //todo: make optional
 				Description: "The ARN of the AWS IAM role used to access this AWS account",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"eventbus_name": schema.StringAttribute{
+				Computed:    true,
+				Description: "The name of CrowdStrike Event Bridge to forward messages to",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"eventbus_arn": schema.StringAttribute{
+				Computed:    true,
+				Description: "The ARN of CrowdStrike Event Bridge to forward messages to",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"cloudtrail_bucket_name": schema.StringAttribute{
+				Computed:    true,
+				Description: "",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"cloudtrail_region": schema.StringAttribute{
+				Optional:    true,
+				Description: "The AWS region of the CloudTrail bucket",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+				Validators: []validator.String{},
+			},
+			"dspm_role_name": schema.StringAttribute{
+				Optional:    true,
+				Description: "The name of the IAM role to be used by CrowdStrike DSPM",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"dspm_role_arn": schema.StringAttribute{
+				Computed:    true,
+				Optional:    true,
+				Description: "The ARN of the IAM role to be used by CrowdStrike DSPM",
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.UseStateForUnknown(),
 				},
@@ -172,6 +239,11 @@ func (r *cspmAWSAccountResource) Create(
 	plan.ExternalID = types.StringValue(account.ExternalID)
 	plan.IntermediateRoleArn = types.StringValue(account.IntermediateRoleArn)
 	plan.IamRoleArn = types.StringValue(account.IamRoleArn)
+	plan.EventbusName = types.StringValue(account.EventbusName)
+	plan.EventbusArn = types.StringValue(account.AwsEventbusArn)
+	plan.CloudTrailBucketName = types.StringValue(account.AwsCloudtrailBucketName)
+	plan.CloudTrailRegion = types.StringValue(account.AwsCloudtrailRegion)
+	plan.DSPMRoleArn = types.StringValue(account.DspmRoleArn)
 
 	//todo: add other fields
 
@@ -207,13 +279,19 @@ func (r *cspmAWSAccountResource) Read(
 	if account != nil {
 		state.AccountID = types.StringValue(account.AccountID)
 		state.OrganizationID = types.StringValue(account.OrganizationID)
-		//todo: add other fields
+		state.TargetOUs, diags = types.ListValueFrom(ctx, types.StringType, account.TargetOus)
+		resp.Diagnostics.Append(diags...)
 		state.EnableRealtimeVisibility = types.BoolValue(account.BehaviorAssessmentEnabled)
 		state.EnableSensorManagement = types.BoolValue(*account.SensorManagementEnabled)
 		state.EnableDSPM = types.BoolValue(account.DspmEnabled)
 		state.ExternalID = types.StringValue(account.ExternalID)
 		state.IntermediateRoleArn = types.StringValue(account.IntermediateRoleArn)
 		state.IamRoleArn = types.StringValue(account.IamRoleArn)
+		state.EventbusName = types.StringValue(account.EventbusName)
+		state.EventbusArn = types.StringValue(account.AwsEventbusArn)
+		state.CloudTrailBucketName = types.StringValue(account.AwsCloudtrailBucketName)
+		state.CloudTrailRegion = types.StringValue(account.AwsCloudtrailRegion)
+		state.DSPMRoleArn = types.StringValue(account.DspmRoleArn)
 	}
 
 	// Set refreshed state
@@ -255,7 +333,17 @@ func (r *cspmAWSAccountResource) Update(
 	if account.OrganizationID != "" {
 		plan.OrganizationID = types.StringValue(account.OrganizationID)
 	}
-	// todo: add other fields
+	plan.EnableRealtimeVisibility = types.BoolValue(account.BehaviorAssessmentEnabled)
+	plan.EnableSensorManagement = types.BoolValue(*account.SensorManagementEnabled)
+	plan.EnableDSPM = types.BoolValue(account.DspmEnabled)
+	plan.ExternalID = types.StringValue(account.ExternalID)
+	plan.IntermediateRoleArn = types.StringValue(account.IntermediateRoleArn)
+	plan.IamRoleArn = types.StringValue(account.IamRoleArn)
+	plan.EventbusName = types.StringValue(account.EventbusName)
+	plan.EventbusArn = types.StringValue(account.AwsEventbusArn)
+	plan.CloudTrailBucketName = types.StringValue(account.AwsCloudtrailBucketName)
+	plan.CloudTrailRegion = types.StringValue(account.AwsCloudtrailRegion)
+	plan.DSPMRoleArn = types.StringValue(account.DspmRoleArn)
 
 	diags = resp.State.Set(ctx, plan)
 	resp.Diagnostics.Append(diags...)
@@ -329,94 +417,6 @@ func (r *cspmAWSAccountResource) ValidateConfig(
 	if resp.Diagnostics.HasError() {
 		return
 	}
-
-	// for i, exclusion := range config.ScheduledExclusions {
-	// 	repeated := exclusion.Repeated
-	// 	attrPath := path.Root("scheduled_exclusions").AtListIndex(i)
-	//
-	// 	_, err := time.LoadLocation(exclusion.Timezone.ValueString())
-	// 	if err != nil {
-	// 		resp.Diagnostics.AddAttributeError(
-	// 			attrPath,
-	// 			"Invalid timezone in scheduled exclusion",
-	// 			"Invalid timezone see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for valid timezones.",
-	// 		)
-	// 	}
-	//
-	// 	resp.Diagnostics.Append(
-	// 		valideDate(attrPath, exclusion.StartDate.ValueString())...)
-	// 	resp.Diagnostics.Append(
-	// 		valideDate(attrPath, exclusion.EndDate.ValueString())...)
-	// 	resp.Diagnostics.Append(
-	// 		valideTime(attrPath, exclusion.StartTime.ValueString())...)
-	// 	resp.Diagnostics.Append(
-	// 		valideTime(attrPath, exclusion.EndTime.ValueString())...)
-	//
-	// 	// validate repeated
-	// 	if repeated == nil {
-	// 		continue
-	// 	}
-	//
-	// 	resp.Diagnostics.Append(
-	// 		valideTime(attrPath, repeated.StartTime.ValueString())...)
-	// 	resp.Diagnostics.Append(
-	// 		valideTime(attrPath, repeated.EndTime.ValueString())...)
-	//
-	// 	summaryMsg := "Invalid repeated attribute on scheduled exclusion"
-	//
-	// 	if !repeated.AllDay.ValueBool() && repeated.StartTime.ValueString() == "" {
-	// 		resp.Diagnostics.AddAttributeError(
-	// 			attrPath,
-	// 			summaryMsg,
-	// 			"start_time is required if all_day is false",
-	// 		)
-	// 	}
-	// 	if !repeated.AllDay.ValueBool() && repeated.EndTime.ValueString() == "" {
-	// 		resp.Diagnostics.AddAttributeError(
-	// 			attrPath,
-	// 			summaryMsg,
-	// 			"end_time is required in repeated if all_day is false",
-	// 		)
-	// 	}
-	//
-	// 	// required attributes for when frequency is weekly
-	// 	if repeated.Frequency.ValueString() == "weekly" &&
-	// 		len(repeated.DaysOfWeek.Elements()) == 0 {
-	// 		resp.Diagnostics.AddAttributeError(
-	// 			attrPath,
-	// 			summaryMsg,
-	// 			"days_of_week is required in repeated if frequency is weekly",
-	// 		)
-	// 	}
-	//
-	// 	// required attributes for when frequency is monthly
-	// 	if repeated.Frequency.ValueString() == "monthly" {
-	// 		switch repeated.MonthlyOccurrence.ValueString() {
-	// 		case "":
-	// 			resp.Diagnostics.AddAttributeError(
-	// 				attrPath,
-	// 				summaryMsg,
-	// 				"monthly_occurrence is required in repeated if frequency is monthly",
-	// 			)
-	// 		case "Days":
-	// 			if len(repeated.DaysOfMonth.Elements()) == 0 {
-	// 				resp.Diagnostics.AddAttributeError(
-	// 					attrPath,
-	// 					summaryMsg,
-	// 					"days_of_month is required in repeated if frequency is monthly and monthly_occurrence is days",
-	// 				)
-	// 			}
-	// 		case "1st", "2nd", "3rd", "4th", "Last":
-	// 			if len(repeated.DaysOfWeek.Elements()) == 0 {
-	// 				resp.Diagnostics.AddAttributeError(
-	// 					attrPath,
-	// 					summaryMsg,
-	// 					"days_of_week is required in repeated if frequency is monthly and monthly_occurrence is set to a week",
-	// 				)
-	// 			}
-	// 		}
-	// 	}
-	// }
 }
 
 // createAccount creates a new CSPM AWS account from the resource model.
@@ -425,13 +425,16 @@ func (r *cspmAWSAccountResource) createAccount(
 	config cspmAWSAccountModel,
 ) (*models.DomainAWSAccountV2, diag.Diagnostics) {
 	var diags diag.Diagnostics
+	var targetOUs []string
 
+	diags.Append(config.TargetOUs.ElementsAs(ctx, &targetOUs, false)...)
 	tflog.Debug(ctx, "creating cspm aws account", map[string]interface{}{
 		"account_id":                  config.AccountID.ValueString(),
 		"organization_id":             config.OrganizationID.ValueString(),
 		"behavior_assessment_enabled": config.EnableRealtimeVisibility.ValueBool(),
+		"cloudtrail_region":           config.CloudTrailRegion.ValueString(),
+		"target_ous":                  targetOUs,
 	})
-
 	res, status, err := r.client.CspmRegistration.CreateCSPMAwsAccount(&cspm_registration.CreateCSPMAwsAccountParams{
 		Context: ctx,
 		Body: &models.RegistrationAWSAccountCreateRequestExtV2{
@@ -439,9 +442,13 @@ func (r *cspmAWSAccountResource) createAccount(
 				{
 					AccountID:                 config.AccountID.ValueStringPointer(),
 					OrganizationID:            config.OrganizationID.ValueStringPointer(),
+					TargetOus:                 targetOUs,
+					CloudtrailRegion:          config.CloudTrailRegion.ValueStringPointer(),
 					BehaviorAssessmentEnabled: config.EnableRealtimeVisibility.ValueBool(),
 					SensorManagementEnabled:   config.EnableSensorManagement.ValueBool(),
 					DspmEnabled:               config.EnableDSPM.ValueBool(),
+					DspmRole:                  config.DSPMRoleName.ValueString(),
+					DeploymentMethod:          "terraform-native",
 				},
 			},
 		},
@@ -514,47 +521,49 @@ func (r *cspmAWSAccountResource) updateAccount(
 	account cspmAWSAccountModel,
 ) (*models.DomainAWSAccountV2, diag.Diagnostics) {
 	var diags diag.Diagnostics
-	r.client.CspmRegistration.PatchCSPMAwsAccount(&cspm_registration.PatchCSPMAwsAccountParams{
-		// res, status, err := r.client.CspmRegistration.PatchCSPMAwsAccount(&cspm_registration.PatchCSPMAwsAccountParams{
+	var targetOUs []string
+
+	diags.Append(account.TargetOUs.ElementsAs(ctx, &targetOUs, false)...)
+	res, status, err := r.client.CspmRegistration.PatchCSPMAwsAccount(&cspm_registration.PatchCSPMAwsAccountParams{
 		Context: ctx,
 		Body: &models.RegistrationAWSAccountPatchRequest{
 			Resources: []*models.RegistrationAWSAccountPatch{
 				{
-					AccountID: account.AccountID.ValueStringPointer(),
+					AccountID:                 account.AccountID.ValueStringPointer(),
+					TargetOus:                 targetOUs,
+					CloudtrailRegion:          account.CloudTrailRegion.ValueString(),
+					BehaviorAssessmentEnabled: account.EnableRealtimeVisibility.ValueBool(),
+					SensorManagementEnabled:   account.EnableSensorManagement.ValueBool(),
+					DspmEnabled:               account.EnableDSPM.ValueBool(),
+					DspmRole:                  account.DSPMRoleName.ValueString(),
 				},
 			},
 		},
 	})
 
-	// TODO: remove this when gofalcon is fixed and revert to previous code
-	return &models.DomainAWSAccountV2{
-		AccountID:      account.AccountID.ValueString(),
-		OrganizationID: account.OrganizationID.ValueString(),
-	}, diags
+	if err != nil {
+		diags.AddError(
+			"Failed to update CSPM AWS account",
+			fmt.Sprintf("Failed to update CSPM AWS account: %s", err.Error()),
+		)
+		return nil, diags
+	}
+	if status != nil {
+		diags.AddError(
+			"Failed to update CSPM AWS account",
+			fmt.Sprintf("Failed to update CSPM AWS account: %s", status.Error()),
+		)
+		return nil, diags
+	}
 
-	// if err != nil {
-	// 	diags.AddError(
-	// 		"Failed to update CSPM AWS account",
-	// 		fmt.Sprintf("Failed to update CSPM AWS account: %s", err.Error()),
-	// 	)
-	// 	return nil, diags
-	// }
-	// if status != nil {
-	// 	diags.AddError(
-	// 		"Failed to update CSPM AWS account",
-	// 		fmt.Sprintf("Failed to update CSPM AWS account: %s", status.Error()),
-	// 	)
-	// 	return nil, diags
-	// }
-
-	// if len(res.Payload.Resources) == 0 {
-	// 	diags.AddError(
-	// 		"Failed to update CSPM AWS account",
-	// 		"No error returned from api but CSPM account was not returned. Please report this issue to the provider developers.",
-	// 	)
-	// 	return nil, diags
-	// }
-	// return res.Payload.Resources[0], diags
+	if len(res.Payload.Resources) == 0 {
+		diags.AddError(
+			"Failed to update CSPM AWS account",
+			"No error returned from api but CSPM account was not returned. Please report this issue to the provider developers.",
+		)
+		return nil, diags
+	}
+	return res.Payload.Resources[0], diags
 }
 
 func (r *cspmAWSAccountResource) deleteAccount(
