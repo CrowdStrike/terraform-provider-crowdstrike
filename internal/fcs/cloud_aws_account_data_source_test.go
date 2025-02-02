a9119fd262db14c108ccdbeb080b56baf3827cc2
Alexandru Gandrabur <alexandru.gandrabur@crowdstrike.com>
2025-02-02 16:01:24 +0200

    update acceptance tests

--- internal/fcs/cloud_aws_account_data_source_test.go
+++ internal/fcs/cloud_aws_account_data_source_test.go
@@ -1,77 +1,99 @@
 package fcs_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/crowdstrike/terraform-provider-crowdstrike/internal/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
+const test_account_id = "123456789111"
+const test_org_account_id = "123456789222"
+const test_org_id = "o-tforgdataa"
+
 func TestAccCloudAwsAccountDataSource(t *testing.T) {
-	dataSourceName := "data.crowdstrike_cloud_aws_accounts.test"
-	resourceName := "crowdstrike_cloud_aws_account.this"
+	dataSourceNameAcc := "data.crowdstrike_cloud_aws_accounts.acc"
+	dataSourceNameOrg := "data.crowdstrike_cloud_aws_accounts.org"
 
 	resource.ParallelTest(t, resource.TestCase{
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
 		PreCheck:                 func() { acctest.PreCheck(t) },
 		Steps: []resource.TestStep{
-			// First create the resources
-			{
-				Config: testAccCloudAwsAccountDataSource_resource(),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "account_id", "123456789030"),
-					resource.TestCheckResourceAttr(resourceName, "csp_events", "true"),
-				),
-			},
 			// Test data source by account_id
 			{
 				Config: testAccCloudAwsAccountDataSource_byAccountID(),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(dataSourceName, "account.account_id", "123456789030"),
-					resource.TestCheckResourceAttr(dataSourceName, "account.account_type", "commercial"),
-					resource.TestCheckResourceAttr(dataSourceName, "account.csp_events", "true"),
-					resource.TestCheckResourceAttr(dataSourceName, "account.is_organization_management_account", "false"),
-					resource.TestCheckResourceAttr(dataSourceName, "account.organization_id", ""),
+					// Check resource was created
+					resource.TestCheckResourceAttr("crowdstrike_cloud_aws_account.acc", "account_id", test_account_id),
 
-					// Verify products
-					resource.TestCheckResourceAttr(dataSourceName, "account.products.#", "1"),
-					resource.TestCheckResourceAttr(dataSourceName, "account.products.0.product", "idp"),
-					resource.TestCheckResourceAttr(dataSourceName, "account.products.0.features.#", "1"),
-					resource.TestCheckResourceAttr(dataSourceName, "account.products.0.features.0", "default"),
+					resource.TestCheckResourceAttr(dataSourceNameAcc, "accounts.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceNameAcc, "accounts.0.account_id", test_account_id),
+					resource.TestCheckResourceAttr(dataSourceNameAcc, "accounts.0.organization_id", ""),
+					resource.TestCheckResourceAttr(dataSourceNameAcc, "accounts.0.is_organization_management_account", "false"),
+					resource.TestCheckResourceAttr(dataSourceNameAcc, "accounts.0.account_type", "commercial"),
+					// Computed fields should exist
+					resource.TestCheckResourceAttrSet(dataSourceNameAcc, "accounts.0.external_id"),
+					resource.TestCheckResourceAttrSet(dataSourceNameAcc, "accounts.0.intermediate_role_arn"),
+					resource.TestCheckResourceAttrSet(dataSourceNameAcc, "accounts.0.iam_role_arn"),
+					resource.TestCheckResourceAttrSet(dataSourceNameAcc, "accounts.0.eventbus_name"),
+					resource.TestCheckResourceAttrSet(dataSourceNameAcc, "accounts.0.eventbus_arn"),
+					resource.TestCheckResourceAttrSet(dataSourceNameAcc, "accounts.0.dspm_role_arn"),
+				),
+			},
+			// Test data source by organization_id
+			{
+				Config: testAccCloudAwsAccountDataSource_byOrgID(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Check resource was created
+					resource.TestCheckResourceAttr("crowdstrike_cloud_aws_account.org", "account_id", test_org_account_id),
+
+					resource.TestCheckResourceAttr(dataSourceNameOrg, "accounts.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceNameOrg, "accounts.0.account_id", test_org_account_id),
+					resource.TestCheckResourceAttr(dataSourceNameOrg, "accounts.0.organization_id", test_org_id),
+					resource.TestCheckResourceAttr(dataSourceNameOrg, "accounts.0.is_organization_management_account", "true"),
+					resource.TestCheckResourceAttr(dataSourceNameOrg, "accounts.0.account_type", "commercial"),
+					// Computed fields should exist
+					resource.TestCheckResourceAttrSet(dataSourceNameOrg, "accounts.0.external_id"),
+					resource.TestCheckResourceAttrSet(dataSourceNameOrg, "accounts.0.intermediate_role_arn"),
+					resource.TestCheckResourceAttrSet(dataSourceNameOrg, "accounts.0.iam_role_arn"),
+					resource.TestCheckResourceAttrSet(dataSourceNameOrg, "accounts.0.eventbus_name"),
+					resource.TestCheckResourceAttrSet(dataSourceNameOrg, "accounts.0.eventbus_arn"),
+					resource.TestCheckResourceAttrSet(dataSourceNameOrg, "accounts.0.dspm_role_arn"),
 				),
 			},
 		},
 	})
 }
 
-func testAccCloudAwsAccountDataSource_resource() string {
-	return `
-resource "crowdstrike_cspm_aws_account" "this" {
-    account_id                         = "123456789030"
-    cloudtrail_region                  = "us-east-1"
-    enable_realtime_visibility         = true
-}
-
-resource "crowdstrike_cloud_aws_account" "this" {
-    account_id = "123456789030"
-    csp_events = true
-    products = [
-        {
-            product  = "idp"
-            features = ["default"]
-        }
-    ]
-    depends_on = [
-        crowdstrike_cspm_aws_account.this
-    ]
-}
-`
-}
-
 func testAccCloudAwsAccountDataSource_byAccountID() string {
-	return `
-data "crowdstrike_cloud_aws_accounts" "test" {
-    account_id = "123456789030"
+	return fmt.Sprintf(`
+resource "crowdstrike_cloud_aws_account" "acc" {
+  account_id                         = "%s"
 }
-`
+
+data "crowdstrike_cloud_aws_accounts" "acc" {
+  account_id = "%s"
+  depends_on = [
+    crowdstrike_cloud_aws_account.acc
+  ]
+}
+`, test_account_id, test_account_id)
+}
+
+func testAccCloudAwsAccountDataSource_byOrgID() string {
+	return fmt.Sprintf(`
+resource "crowdstrike_cloud_aws_account" "org" {
+  account_id                         = "%s"
+  organization_id                    = "%s"
+  is_organization_management_account = true
+}
+
+data "crowdstrike_cloud_aws_accounts" "org" {
+  organization_id = "%s"
+  depends_on = [
+    crowdstrike_cloud_aws_account.org
+  ]
+}
+`, test_org_account_id, test_org_id, test_org_id)
 }
--- internal/fcs/cloud_aws_account_test.go
+++ internal/fcs/cloud_aws_account_test.go
@@ -1,11 +1,14 @@
 package fcs_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/crowdstrike/terraform-provider-crowdstrike/internal/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
+
+const account_id = "123456789000"
 
 func TestAccCloudAwsAccountResource(t *testing.T) {
 	resourceName := "crowdstrike_cloud_aws_account.test"
@@ -16,46 +19,73 @@ func TestAccCloudAwsAccountResource(t *testing.T) {
 		Steps: []resource.TestStep{
 			// Create and Read testing
 			{
-				Config: testAccCloudAwsAccountConfig_basic(),
+				Config: testAccCloudAwsAccountConfig_basic(account_id),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "account_id", "123456789010"),
+					resource.TestCheckResourceAttr(resourceName, "account_id", account_id),
 					resource.TestCheckResourceAttr(resourceName, "organization_id", "o-tfacctestt"),
 					resource.TestCheckResourceAttr(resourceName, "is_organization_management_account", "true"),
-					resource.TestCheckResourceAttr(resourceName, "csp_events", "false"),
-					// Test products list
-					resource.TestCheckResourceAttr(resourceName, "products.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "account_type", "commercial"),
+					resource.TestCheckResourceAttr(resourceName, "target_ous.#", "2"),
+					// Computed fields should exist
+					resource.TestCheckResourceAttrSet(resourceName, "external_id"),
+					resource.TestCheckResourceAttrSet(resourceName, "intermediate_role_arn"),
+					resource.TestCheckResourceAttrSet(resourceName, "iam_role_arn"),
+					resource.TestCheckResourceAttrSet(resourceName, "eventbus_name"),
+					resource.TestCheckResourceAttrSet(resourceName, "eventbus_arn"),
+					resource.TestCheckResourceAttrSet(resourceName, "dspm_role_arn"),
 				),
 			},
 			// Import testing
 			{
 				ResourceName:                         resourceName,
 				ImportState:                          true,
-				ImportStateId:                        "123456789010",
+				ImportStateId:                        account_id,
 				ImportStateVerify:                    true,
 				ImportStateVerifyIdentifierAttribute: "account_id",
 				ImportStateVerifyIgnore: []string{
 					"id",
+					"deployment_method",
+					"target_ous",
+					"asset_inventory",
+					"realtime_visibility",
+					"idp",
+					"sensor_management",
+					"dspm",
 				},
 			},
 			// Update testing
 			{
-				Config: testAccCloudAwsAccountConfig_update(),
+				Config: testAccCloudAwsAccountConfig_update(account_id),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "account_id", "123456789010"),
-					resource.TestCheckResourceAttr(resourceName, "csp_events", "true"),
-					// Test updated products
-					resource.TestCheckResourceAttr(resourceName, "products.0.product", "idp"),
-					resource.TestCheckResourceAttr(resourceName, "products.0.features.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "products.0.features.0", "default"),
+					resource.TestCheckResourceAttr(resourceName, "account_id", account_id),
+					resource.TestCheckResourceAttr(resourceName, "organization_id", "o-tfacctestt"),
+					resource.TestCheckResourceAttr(resourceName, "is_organization_management_account", "true"),
+					resource.TestCheckResourceAttr(resourceName, "account_type", "commercial"),
+					resource.TestCheckResourceAttr(resourceName, "target_ous.#", "2"),
+					// Computed fields should exist
+					resource.TestCheckResourceAttrSet(resourceName, "external_id"),
+					resource.TestCheckResourceAttrSet(resourceName, "intermediate_role_arn"),
+					resource.TestCheckResourceAttrSet(resourceName, "iam_role_arn"),
+					resource.TestCheckResourceAttrSet(resourceName, "eventbus_name"),
+					resource.TestCheckResourceAttrSet(resourceName, "eventbus_arn"),
+					// resource.TestCheckResourceAttrSet(resourceName, "cloudtrail_bucket_name"),
+					resource.TestCheckResourceAttrSet(resourceName, "dspm_role_arn"),
 				),
 			},
 			// Test minimal configuration
 			{
-				Config: testAccCloudAwsAccountConfig_minimal(),
+				Config: testAccCloudAwsAccountConfig_minimal("123456789333"),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "account_id", "123456789020"),
-					resource.TestCheckResourceAttr(resourceName, "csp_events", "true"),
-					resource.TestCheckResourceAttr(resourceName, "products.#", "0"),
+					resource.TestCheckResourceAttr(resourceName, "account_id", "123456789333"),
+					resource.TestCheckResourceAttr(resourceName, "is_organization_management_account", "false"),
+					resource.TestCheckResourceAttr(resourceName, "account_type", "commercial"),
+					// Computed fields should exist
+					resource.TestCheckResourceAttrSet(resourceName, "external_id"),
+					resource.TestCheckResourceAttrSet(resourceName, "intermediate_role_arn"),
+					resource.TestCheckResourceAttrSet(resourceName, "iam_role_arn"),
+					resource.TestCheckResourceAttrSet(resourceName, "eventbus_name"),
+					resource.TestCheckResourceAttrSet(resourceName, "eventbus_arn"),
+					resource.TestCheckResourceAttrSet(resourceName, "dspm_role_arn"),
 				),
 			},
 		},
@@ -63,76 +93,53 @@ func TestAccCloudAwsAccountResource(t *testing.T) {
 }
 
 // Basic configuration.
-func testAccCloudAwsAccountConfig_basic() string {
-	return `
-resource "crowdstrike_cspm_aws_account" "this" {
-    account_id                         = "123456789010"
-    organization_id                    = "o-tfacctestt"
-    is_organization_management_account = true
-    cloudtrail_region                  = "us-east-1"
-    enable_realtime_visibility         = true
-    deployment_method                  = "terraform-native"
-}
-
+func testAccCloudAwsAccountConfig_basic(account string) string {
+	return fmt.Sprintf(`
 resource "crowdstrike_cloud_aws_account" "test" {
-    account_id                         = "123456789010"
-    organization_id                    = "o-tfacctestt"
-    is_organization_management_account = true
-    csp_events                         = false
-    products = [
-    ]
-    depends_on = [
-        crowdstrike_cspm_aws_account.this
-    ]
+  account_id                         = "%s"
+  organization_id                    = "o-tfacctestt"
+  is_organization_management_account = true
+  target_ous                         = ["ou-abcd-defghijk", "r-abcd"]
+  account_type                       = "commercial"
 }
-`
+`, account)
 }
 
 // Updated configuration with multiple products.
-func testAccCloudAwsAccountConfig_update() string {
-	return `
-resource "crowdstrike_cspm_aws_account" "this" {
-    account_id                         = "123456789010"
-    organization_id                    = "o-tfacctestt"
-    is_organization_management_account = true
-    cloudtrail_region                  = "us-east-1"
-    enable_realtime_visibility         = true
-    deployment_method                  = "terraform-native"
-}
-
+func testAccCloudAwsAccountConfig_update(account string) string {
+	return fmt.Sprintf(`
 resource "crowdstrike_cloud_aws_account" "test" {
-    account_id                         = "123456789010"
-    organization_id                    = "o-tfacctestt"
-    is_organization_management_account = true
-    csp_events                         = true
-    products = [
-        {
-            product  = "idp"
-            features = ["default"]
-        }
-    ]
-    depends_on = [
-        crowdstrike_cspm_aws_account.this
-    ]
+  account_id                         = "%s"
+  organization_id                    = "o-tfacctestt"
+  is_organization_management_account = true
+  target_ous                         = ["ou-abcd-defghijk", "r-abcd"]
+  account_type                       = "commercial"
+  asset_inventory = {
+    enabled   = true
+  }
+  realtime_visibility = {
+    enabled           = true
+    cloudtrail_region = "us-east-1"
+  }
+  idp = {
+    enabled = true
+  }
+  sensor_management = {
+    enabled = true
+  }
+  dspm = {
+    enabled = true
+    role_name = "mydspmrole"
+  }
 }
-`
+`, account)
 }
 
 // Minimal configuration with only required attributes.
-func testAccCloudAwsAccountConfig_minimal() string {
-	return `
-resource "crowdstrike_cspm_aws_account" "this" {
-    account_id                         = "123456789020"
-    cloudtrail_region                  = "us-east-1"
-    enable_realtime_visibility         = true
-}
-
+func testAccCloudAwsAccountConfig_minimal(account string) string {
+	return fmt.Sprintf(`
 resource "crowdstrike_cloud_aws_account" "test" {
-    account_id = "123456789020"
-    csp_events = true
-    depends_on = [
-        crowdstrike_cspm_aws_account.this
-    ]
+    account_id                         = "%s"
 }
-`
+`, account)
 }
