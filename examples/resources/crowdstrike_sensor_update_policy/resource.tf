1149749cd2df2047ef0bd44439280c4b4a4d4d87
ffalor <35144141+ffalor@users.noreply.github.com>
2025-03-21 13:58:06 -0500

    u

--- examples/resources/crowdstrike_sensor_update_policy/resource.tf
+++ examples/resources/crowdstrike_sensor_update_policy/resource.tf
@@ -13,12 +13,11 @@ provider "crowdstrike" {
 data "crowdstrike_sensor_update_policy_builds" "all" {}
 
 resource "crowdstrike_sensor_update_policy" "example" {
-  name          = "example_prevention_policy"
-  enabled       = false
-  description   = "made with terraform"
-  platform_name = "Windows"
-  build         = data.crowdstrike_sensor_update_policy_builds.all.windows.n1.build
-  # build                = "1"
+  name                 = "example_prevention_policy"
+  enabled              = false
+  description          = "made with terraform"
+  platform_name        = "Windows"
+  build                = data.crowdstrike_sensor_update_policy_builds.all.windows.n1.build
   uninstall_protection = false
   host_groups          = ["host_group_id"]
   schedule = {
