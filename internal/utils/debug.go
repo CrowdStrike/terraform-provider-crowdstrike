c2c8221b9e326901bda2156298e439ded596fedf
ffalor <35144141+ffalor@users.noreply.github.com>
2025-03-21 22:02:41 -0500

    linting

--- internal/utils/debug.go
+++ internal/utils/debug.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 )
 
-// PancicPrettyPrint converts the interface into a json string and panics
+// PancicPrettyPrint converts the interface into a json string and panics.
 func PancicPrettyPrint(v interface{}) {
 	s, _ := json.MarshalIndent(v, "", "    ")
 	panic(string(s))
