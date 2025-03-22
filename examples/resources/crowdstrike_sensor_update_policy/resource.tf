ec04c879e8988f5c60c34e235eb1aa2105386671
ffalor <35144141+ffalor@users.noreply.github.com>
2025-03-21 21:49:43 -0500

    update sensor_update_policy example

--- examples/resources/crowdstrike_sensor_update_policy/resource.tf
+++ examples/resources/crowdstrike_sensor_update_policy/resource.tf
@@ -10,13 +10,14 @@ provider "crowdstrike" {
   cloud = "us-2"
 }
 
+data "crowdstrike_sensor_update_policy_builds" "all" {}
 
 resource "crowdstrike_sensor_update_policy" "example" {
   name                 = "example_prevention_policy"
   enabled              = false
   description          = "made with terraform"
   platform_name        = "Windows"
-  build                = "18110"
+  build                = data.crowdstrike_sensor_update_policy_builds.all.windows.n1.build
   uninstall_protection = false
   host_groups          = ["host_group_id"]
   schedule = {
