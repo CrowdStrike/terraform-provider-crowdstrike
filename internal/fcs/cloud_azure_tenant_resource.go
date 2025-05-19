3fa4b25d0a942c871ac9ce83d46b62338f692b48
ffalor <35144141+ffalor@users.noreply.github.com>
2025-05-19 12:22:28 -0500

    wip

--- internal/fcs/cloud_azure_tenant_resource.go
+++ internal/fcs/cloud_azure_tenant_resource.go
@@ -2,7 +2,6 @@ package fcs
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 
 	"github.com/crowdstrike/gofalcon/falcon/client"
@@ -10,7 +9,10 @@ import (
 	"github.com/crowdstrike/gofalcon/falcon/models"
 	"github.com/crowdstrike/terraform-provider-crowdstrike/internal/resource_cloud_azure_tenant"
 	"github.com/crowdstrike/terraform-provider-crowdstrike/internal/utils"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
 var (
@@ -71,39 +73,48 @@ func (r *cloudAzureTenantResource) Schema(
 	resp.Schema = resource_cloud_azure_tenant.CloudAzureTenantResourceSchema(ctx)
 }
 
+func wrap(
+	ctx context.Context,
+	model *resource_cloud_azure_tenant.CloudAzureTenantModel,
+	registration models.AzureTenantRegistration,
+) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	model.TenantId = types.StringValue(*registration.TenantID)
+	model.AppRegistrationId = types.StringValue(registration.AppRegistrationID)
+
+	graphPermissionIDs, err := types.ListValueFrom(
+		ctx,
+		types.StringType,
+		registration.MicrosoftGraphPermissionIds,
+	)
+	diags.Append(err...)
+	if diags.HasError() {
+		return diags
+	}
+
+	model.MicrosoftGraphPermissionIds = graphPermissionIDs
+	return diags
+}
+
 func (r *cloudAzureTenantResource) Create(
 	ctx context.Context,
 	req resource.CreateRequest,
 	resp *resource.CreateResponse,
 ) {
 	var data resource_cloud_azure_tenant.CloudAzureTenantModel
-
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Create API call logic
-	_, err := r.client.CloudAzureRegistration.CloudRegistrationAzureCreateRegistration(
-		&cloud_azure_registration.CloudRegistrationAzureCreateRegistrationParams{
-			Body: &models.AzureAzureRegistrationCreateRequestExtV1{
-				Resource: &models.AzureTenantRegistrationBase{
-					TenantID:                    data.TenantId.ValueStringPointer(),
-					MicrosoftGraphPermissionIds: []string{"9a5d68dd-52b0-4cc2-bd40-abcf44ac3a30"},
-					DeploymentMethod:            "terraform-native",
-				},
-			},
-			Context: ctx,
-		},
-	)
-
-	if err != nil {
-		s, _ := json.MarshalIndent(err, "", "    ")
-		panic(string(s))
-
-		utils.PancicPrettyPrint(err.Error())
+	registration, err := r.createRegistration(ctx, &data)
+	resp.Diagnostics.Append(err...)
+	if resp.Diagnostics.HasError() {
+		return
 	}
 
+	resp.Diagnostics.Append(wrap(ctx, &data, *registration)...)
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
@@ -113,14 +124,32 @@ func (r *cloudAzureTenantResource) Read(
 	resp *resource.ReadResponse,
 ) {
 	var data resource_cloud_azure_tenant.CloudAzureTenantModel
-
 	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Read API call logic
+	registration, diags := r.getRegistration(ctx, data.TenantId.ValueString())
+	for _, err := range diags.Errors() {
+		if err.Summary() == notFoundErrorSummary {
+			tflog.Warn(
+				ctx,
+				fmt.Sprintf(
+					"registration for tenant %s not found, removing from state",
+					data.TenantId.ValueString(),
+				),
+			)
 
+			resp.State.RemoveResource(ctx)
+			return
+		}
+	}
+
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	resp.Diagnostics.Append(wrap(ctx, &data, *registration)...)
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
@@ -130,14 +159,18 @@ func (r *cloudAzureTenantResource) Update(
 	resp *resource.UpdateResponse,
 ) {
 	var data resource_cloud_azure_tenant.CloudAzureTenantModel
-
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Update API call logic
+	registration, err := r.updateRegistration(ctx, &data)
+	resp.Diagnostics.Append(err...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
+	resp.Diagnostics.Append(wrap(ctx, &data, *registration)...)
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
@@ -147,11 +180,191 @@ func (r *cloudAzureTenantResource) Delete(
 	resp *resource.DeleteResponse,
 ) {
 	var data resource_cloud_azure_tenant.CloudAzureTenantModel
-
 	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Delete API call logic
+	resp.Diagnostics.Append(r.deleteRegistration(ctx, data.TenantId.ValueString())...)
+}
+
+func (r *cloudAzureTenantResource) deleteRegistration(
+	ctx context.Context,
+	tenantID string,
+) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	_, err := r.client.CloudAzureRegistration.CloudRegistrationAzureDeleteRegistration(
+		&cloud_azure_registration.CloudRegistrationAzureDeleteRegistrationParams{
+			TenantIds: []string{tenantID},
+			Context:   ctx,
+		},
+	)
+
+	if err != nil {
+		diags.AddError(
+			"Failed to delete registration",
+			fmt.Sprintf("Failed to delete azure tenant registration: %s", err),
+		)
+
+		return diags
+	}
+
+	return diags
+}
+
+func (r *cloudAzureTenantResource) getRegistration(
+	ctx context.Context,
+	tenantID string,
+) (*models.AzureTenantRegistration, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	res, err := r.client.CloudAzureRegistration.CloudRegistrationAzureGetRegistration(
+		&cloud_azure_registration.CloudRegistrationAzureGetRegistrationParams{
+			TenantID: tenantID,
+			Context:  ctx,
+		},
+	)
+
+	if err != nil {
+		if _, ok := err.(*cloud_azure_registration.CloudRegistrationAzureGetRegistrationNotFound); ok {
+			diags.Append(
+				newNotFoundError(
+					fmt.Sprintf("No registration found for tenant: %s.", tenantID),
+				),
+			)
+			return nil, diags
+		}
+
+		diags.AddError(
+			"Failed to get registration",
+			fmt.Sprintf("Failed to get azure tenant registration: %s", err),
+		)
+
+		return nil, diags
+	}
+
+	//TODO fix
+	if res == nil || res.Payload == nil || res.Payload.Resource == nil {
+		diags.AddError(
+			"Failed to get registration",
+			"Get registration api call returned a successful status code, but no registration information was returned. Please report this issue here: https://github.com/CrowdStrike/terraform-provider-crowdstrike/issues",
+		)
+
+		return nil, diags
+	}
+
+	return res.Payload.Resource, diags
+}
+
+func (r *cloudAzureTenantResource) createRegistration(
+	ctx context.Context,
+	data *resource_cloud_azure_tenant.CloudAzureTenantModel,
+) (*models.AzureTenantRegistration, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	microsoftGraphPermissionIDs := utils.ListTypeAs[string](
+		ctx,
+		data.MicrosoftGraphPermissionIds,
+		&diags,
+	)
+
+	if diags.HasError() {
+		return nil, diags
+	}
+
+	res, err := r.client.CloudAzureRegistration.CloudRegistrationAzureCreateRegistration(
+		&cloud_azure_registration.CloudRegistrationAzureCreateRegistrationParams{
+			Body: &models.AzureAzureRegistrationCreateRequestExtV1{
+				Resource: &models.AzureTenantRegistrationBase{
+					TenantID:                    data.TenantId.ValueStringPointer(),
+					MicrosoftGraphPermissionIds: microsoftGraphPermissionIDs,
+					DeploymentMethod:            "terraform-native",
+				},
+			},
+			Context: ctx,
+		},
+	)
+
+	if err != nil {
+		diags.AddError(
+			"Failed to register tenant",
+			fmt.Sprintf("Failed to register azure tenant: %s", err),
+		)
+
+		return nil, diags
+	}
+
+	if res == nil || res.Payload == nil || res.Payload.Resource == nil {
+		diags.AddError(
+			"Failed to register tenant",
+			"Registration api call returned a successful status code, but no registration information was returned. Please report this issue here: https://github.com/CrowdStrike/terraform-provider-crowdstrike/issues",
+		)
+
+		return nil, diags
+	}
+
+	return res.Payload.Resource, diags
+}
+
+func (r *cloudAzureTenantResource) updateRegistration(
+	ctx context.Context,
+	data *resource_cloud_azure_tenant.CloudAzureTenantModel,
+) (*models.AzureTenantRegistration, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	microsoftGraphPermissionIDs := utils.ListTypeAs[string](
+		ctx,
+		data.MicrosoftGraphPermissionIds,
+		&diags,
+	)
+
+	if diags.HasError() {
+		return nil, diags
+	}
+
+	res, err := r.client.CloudAzureRegistration.CloudRegistrationAzureUpdateRegistration(
+		&cloud_azure_registration.CloudRegistrationAzureUpdateRegistrationParams{
+			Body: &models.AzureAzureRegistrationUpdateRequestExtV1{
+				Resource: &models.AzureTenantRegistrationBase{
+					TenantID:                    data.TenantId.ValueStringPointer(),
+					MicrosoftGraphPermissionIds: microsoftGraphPermissionIDs,
+					DeploymentMethod:            "terraform-native",
+				},
+			},
+			Context: ctx,
+		},
+	)
+
+	if err != nil {
+		if _, ok := err.(*cloud_azure_registration.CloudRegistrationAzureUpdateRegistrationNotFound); ok {
+			diags.Append(
+				newNotFoundError(
+					fmt.Sprintf(
+						"No registration found for tenant: %s.",
+						data.TenantId.ValueString(),
+					),
+				),
+			)
+			return nil, diags
+		}
+
+		diags.AddError(
+			"Failed to update registration",
+			fmt.Sprintf("Failed to update azure tenant registration: %s", err),
+		)
+
+		return nil, diags
+	}
+
+	if res == nil || res.Payload == nil || res.Payload.Resource == nil {
+		diags.AddError(
+			"Failed to update registration",
+			"Update registration api call returned a successful status code, but no registration information was returned. Please report this issue here: https://github.com/CrowdStrike/terraform-provider-crowdstrike/issues",
+		)
+
+		return nil, diags
+	}
+
+	return res.Payload.Resource, diags
 }
