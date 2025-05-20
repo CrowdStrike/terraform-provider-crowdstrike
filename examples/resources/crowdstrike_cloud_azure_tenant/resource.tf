c98681a2fa57ed170ffc153085b20cee7fb69c15
ffalor <35144141+ffalor@users.noreply.github.com>
2025-05-20 15:34:10 -0500

    wip

--- examples/resources/crowdstrike_cloud_azure_tenant/resource.tf
+++ examples/resources/crowdstrike_cloud_azure_tenant/resource.tf
@@ -12,5 +12,9 @@ provider "crowdstrike" {
 
 resource "crowdstrike_cloud_azure_tenant" "org" {
   tenant_id                      = "00000000-0000-0000-0000-000000000000"
-  microsoft_graph_permission_ids = []
+  microsoft_graph_permission_ids = ["9a5d68dd-52b0-4cc2-bd40-abcf44ac3a30"]
+}
+
+output "tenant_registration" {
+  value = crowdstrike_cloud_azure_tenant.org
 }
