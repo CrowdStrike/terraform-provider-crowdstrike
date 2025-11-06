4a473ef8f8dcbd6c7b8e344841379f02d262f876
Ryan Payne <ryan.payne@crowdstrike.com>
2025-11-06 10:59:46 -0600

    content update policy data source

---
+++ examples/data-sources/crowdstrike_content_update_policies_all/data-source.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    crowdstrike = {
+      source = "registry.terraform.io/crowdstrike/crowdstrike"
+    }
+  }
+}
+
+provider "crowdstrike" {
+  cloud = "us-2"
+}
+
+# Get all content update policies
+data "crowdstrike_content_update_policies" "all" {}
