8a33a5e0ffaaa9f3104709917198cf29ac564c84
Matthew Phillips <matthew.phillips@crowdstrike.com>
2025-11-17 11:50:38 -0500

    feat(real_time_visibility_and_detection): adds new s3 log ingestion method

--- internal/fcs/cloud_aws_account_test.go
+++ internal/fcs/cloud_aws_account_test.go
@@ -622,6 +622,7 @@ resource "crowdstrike_cloud_aws_account" "test" {
   account_id = "%s"
   realtime_visibility = {
     enabled                       = true
+    cloudtrail_region            = "us-east-1"
     log_ingestion_method         = "s3"
     log_ingestion_s3_bucket_name = "test-cloudtrail-logs-bucket"
     log_ingestion_sns_topic_arn  = "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"
@@ -636,10 +637,11 @@ resource "crowdstrike_cloud_aws_account" "test" {
   account_id = "%s"
   realtime_visibility = {
     enabled                         = true
+    cloudtrail_region              = "us-east-1"
     log_ingestion_method           = "s3"
     log_ingestion_s3_bucket_name   = "test-cloudtrail-logs-bucket"
     log_ingestion_sns_topic_arn    = "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"
-    log_ingestion_s3_bucket_prefix = "cloudtrail-logs/"
+    log_ingestion_s3_bucket_prefix = "cloudtrail-logs"
     log_ingestion_kms_key_arn      = "arn:aws:kms:us-east-1:123456789012:key/12345678-1234-1234-1234-123456789012"
   }
 }
@@ -652,6 +654,7 @@ resource "crowdstrike_cloud_aws_account" "test" {
   account_id = "%s"
   realtime_visibility = {
     enabled                      = true
+    cloudtrail_region           = "us-east-1"
     log_ingestion_method        = "s3"
     log_ingestion_sns_topic_arn = "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"
   }
@@ -665,6 +668,7 @@ resource "crowdstrike_cloud_aws_account" "test" {
   account_id = "%s"
   realtime_visibility = {
     enabled                       = true
+    cloudtrail_region            = "us-east-1"
     log_ingestion_method         = "s3"
     log_ingestion_s3_bucket_name = "test-cloudtrail-logs-bucket"
   }
@@ -678,6 +682,7 @@ resource "crowdstrike_cloud_aws_account" "test" {
   account_id = "%s"
   realtime_visibility = {
     enabled                       = true
+    cloudtrail_region            = "us-east-1"
     log_ingestion_method         = "s3"
     log_ingestion_s3_bucket_name = "test-cloudtrail-logs-bucket"
     log_ingestion_sns_topic_arn  = "invalid-arn"
@@ -692,6 +697,7 @@ resource "crowdstrike_cloud_aws_account" "test" {
   account_id = "%s"
   realtime_visibility = {
     enabled                       = true
+    cloudtrail_region            = "us-east-1"
     log_ingestion_method         = "s3"
     log_ingestion_s3_bucket_name = "Invalid_Bucket_Name_With_Underscores"
     log_ingestion_sns_topic_arn  = "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"
@@ -706,6 +712,7 @@ resource "crowdstrike_cloud_aws_account" "test" {
   account_id = "%s"
   realtime_visibility = {
     enabled                       = true
+    cloudtrail_region            = "us-east-1"
     log_ingestion_method         = "s3"
     log_ingestion_s3_bucket_name = "test-cloudtrail-logs-bucket"
     log_ingestion_sns_topic_arn  = "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"
@@ -715,18 +722,43 @@ resource "crowdstrike_cloud_aws_account" "test" {
 `, accountID)
 }
 
-func testAccCloudAwsAccountConfig_s3LogIngestionShortPrefix(accountID string) string {
+func testAccCloudAwsAccountConfig_eventBridgeWithS3Settings(accountID string) string {
 	return fmt.Sprintf(`
 resource "crowdstrike_cloud_aws_account" "test" {
   account_id = "%s"
   realtime_visibility = {
-    enabled                         = true
-    log_ingestion_method           = "s3"
-    log_ingestion_s3_bucket_name   = "test-cloudtrail-logs-bucket"
-    log_ingestion_sns_topic_arn    = "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"
-    log_ingestion_s3_bucket_prefix = "x"
+    enabled                        = true
+    cloudtrail_region             = "us-east-1"
+    log_ingestion_method          = "eventbridge"
+    log_ingestion_s3_bucket_name  = "invalid-bucket-with-eventbridge"
+    log_ingestion_sns_topic_arn   = "arn:aws:sns:us-east-1:123456789012:invalid-topic"
   }
 }
+`, accountID)
+}
+
+
+func testAccCloudAwsAccountConfig_eventBridgeLogIngestion(accountID string) string {
+	return fmt.Sprintf(`
+resource "crowdstrike_cloud_aws_account" "test" {
+  account_id = "%s"
+  realtime_visibility = {
+    enabled              = true
+    cloudtrail_region   = "us-east-1"
+    log_ingestion_method = "eventbridge"
+  }
+}
+`, accountID)
+}
+
+func testAccCloudAwsAccountConfig_rtvdDisabled(accountID string) string {
+	return fmt.Sprintf(`
+resource "crowdstrike_cloud_aws_account" "test" {
+  account_id = "%s"
+  realtime_visibility = {
+    enabled           = false
+    cloudtrail_region = "us-east-1"
+  }
 }
 `, accountID)
 }
@@ -758,7 +790,7 @@ func TestAccCloudAWSAccount_S3LogIngestion(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_method", "s3"),
 					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_name", "test-cloudtrail-logs-bucket"),
 					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_sns_topic_arn", "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"),
-					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_prefix", "cloudtrail-logs/"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_prefix", "cloudtrail-logs"),
 					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_kms_key_arn", "arn:aws:kms:us-east-1:123456789012:key/12345678-1234-1234-1234-123456789012"),
 				),
 			},
@@ -801,19 +833,229 @@ func TestAccCloudAWSAccount_S3LogIngestionValidation(t *testing.T) {
 			},
 			{
 				Config:      testAccCloudAwsAccountConfig_s3LogIngestionInvalidSNSArn(accountID),
-				ExpectError: regexp.MustCompile(`Invalid SNS Topic ARN format`),
+				ExpectError: regexp.MustCompile(`Invalid Attribute Value Match`),
 			},
 			{
 				Config:      testAccCloudAwsAccountConfig_s3LogIngestionInvalidBucketName(accountID),
-				ExpectError: regexp.MustCompile(`Invalid S3 bucket name`),
+				ExpectError: regexp.MustCompile(`Invalid Attribute Value Match`),
 			},
 			{
 				Config:      testAccCloudAwsAccountConfig_s3LogIngestionInvalidKMSArn(accountID),
-				ExpectError: regexp.MustCompile(`Invalid KMS Key ARN format`),
+				ExpectError: regexp.MustCompile(`Invalid Attribute Value Match`),
 			},
+		},
+	})
+}
+
+// TestAccCloudAWSAccount_S3LogIngestionBasic tests basic S3 log ingestion functionality
+func TestAccCloudAWSAccount_S3LogIngestionBasic(t *testing.T) {
+	resourceName := "crowdstrike_cloud_aws_account.test"
+	accountID := sdkacctest.RandStringFromCharSet(12, acctest.CharSetNum)
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
 			{
-				Config:      testAccCloudAwsAccountConfig_s3LogIngestionShortPrefix(accountID),
-				ExpectError: regexp.MustCompile(`string length must be at least 2`),
+				Config: testAccCloudAwsAccountConfig_s3LogIngestionRequired(accountID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_method", "s3"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_name", "test-cloudtrail-logs-bucket"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_sns_topic_arn", "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"),
+				),
+			},
+		},
+	})
+}
+
+// TestAccCloudAWSAccount_S3LogIngestionCreateWithOptional tests if API returns optional fields when set during CREATE
+func TestAccCloudAWSAccount_S3LogIngestionCreateWithOptional(t *testing.T) {
+	resourceName := "crowdstrike_cloud_aws_account.test"
+	accountID := sdkacctest.RandStringFromCharSet(12, acctest.CharSetNum)
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudAwsAccountConfig_s3LogIngestionComplete(accountID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_method", "s3"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_name", "test-cloudtrail-logs-bucket"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_sns_topic_arn", "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_prefix", "cloudtrail-logs"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_kms_key_arn", "arn:aws:kms:us-east-1:123456789012:key/12345678-1234-1234-1234-123456789012"),
+				),
+			},
+		},
+	})
+}
+
+// TestAccCloudAWSAccount_EventBridgeLogIngestion tests explicit EventBridge log ingestion
+func TestAccCloudAWSAccount_EventBridgeLogIngestion(t *testing.T) {
+	resourceName := "crowdstrike_cloud_aws_account.test"
+	accountID := sdkacctest.RandStringFromCharSet(12, acctest.CharSetNum)
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudAwsAccountConfig_eventBridgeLogIngestion(accountID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_method", "eventbridge"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.cloudtrail_region", "us-east-1"),
+					// S3 fields should not be set when using EventBridge
+					resource.TestCheckNoResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_name"),
+					resource.TestCheckNoResourceAttr(resourceName, "realtime_visibility.log_ingestion_sns_topic_arn"),
+				),
+			},
+		},
+	})
+}
+
+// TestAccCloudAWSAccount_LogIngestionMethodSwitching tests switching between log ingestion methods
+func TestAccCloudAWSAccount_LogIngestionMethodSwitching(t *testing.T) {
+	resourceName := "crowdstrike_cloud_aws_account.test"
+	accountID := sdkacctest.RandStringFromCharSet(12, acctest.CharSetNum)
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
+			// Step 1: Create with EventBridge
+			{
+				Config: testAccCloudAwsAccountConfig_eventBridgeLogIngestion(accountID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_method", "eventbridge"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.cloudtrail_region", "us-east-1"),
+				),
+			},
+			// Step 2: Switch to S3 method
+			{
+				Config: testAccCloudAwsAccountConfig_s3LogIngestionRequired(accountID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_method", "s3"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_name", "test-cloudtrail-logs-bucket"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_sns_topic_arn", "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"),
+				),
+			},
+			// Step 3: Switch back to EventBridge
+			{
+				Config: testAccCloudAwsAccountConfig_eventBridgeLogIngestion(accountID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_method", "eventbridge"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.cloudtrail_region", "us-east-1"),
+				),
+			},
+		},
+	})
+}
+
+// TestAccCloudAWSAccount_S3LogIngestionExpansion tests expanding S3 configuration
+func TestAccCloudAWSAccount_S3LogIngestionExpansion(t *testing.T) {
+	resourceName := "crowdstrike_cloud_aws_account.test"
+	accountID := sdkacctest.RandStringFromCharSet(12, acctest.CharSetNum)
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
+			// Step 1: Create with S3 (complete configuration)
+			{
+				Config: testAccCloudAwsAccountConfig_s3LogIngestionComplete(accountID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_method", "s3"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_name", "test-cloudtrail-logs-bucket"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_sns_topic_arn", "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_prefix", "cloudtrail-logs"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_kms_key_arn", "arn:aws:kms:us-east-1:123456789012:key/12345678-1234-1234-1234-123456789012"),
+				),
+			},
+			// Step 2: Switch to minimal S3 configuration (remove optional fields)
+			{
+				Config: testAccCloudAwsAccountConfig_s3LogIngestionRequired(accountID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_method", "s3"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_name", "test-cloudtrail-logs-bucket"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_sns_topic_arn", "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"),
+					// Optional fields should be absent/null when not specified
+					resource.TestCheckNoResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_prefix"),
+					resource.TestCheckNoResourceAttr(resourceName, "realtime_visibility.log_ingestion_kms_key_arn"),
+				),
+			},
+		},
+	})
+}
+
+// TestAccCloudAWSAccount_S3LogIngestionDisableRTVD tests S3 → disabled RTVD → re-enabled transitions
+func TestAccCloudAWSAccount_S3LogIngestionDisableRTVD(t *testing.T) {
+	resourceName := "crowdstrike_cloud_aws_account.test"
+	accountID := sdkacctest.RandStringFromCharSet(12, acctest.CharSetNum)
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
+			// Step 1: Create with S3 log ingestion enabled
+			{
+				Config: testAccCloudAwsAccountConfig_s3LogIngestionRequired(accountID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_method", "s3"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_name", "test-cloudtrail-logs-bucket"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_sns_topic_arn", "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"),
+				),
+			},
+			// Step 2: Disable RTVD entirely
+			{
+				Config: testAccCloudAwsAccountConfig_rtvdDisabled(accountID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.enabled", "false"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.cloudtrail_region", "us-east-1"),
+				),
+			},
+			// Step 3: Re-enable RTVD with EventBridge method
+			{
+				Config: testAccCloudAwsAccountConfig_eventBridgeLogIngestion(accountID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_method", "eventbridge"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.cloudtrail_region", "us-east-1"),
+					// S3 fields should not be set when using EventBridge
+					resource.TestCheckNoResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_name"),
+					resource.TestCheckNoResourceAttr(resourceName, "realtime_visibility.log_ingestion_sns_topic_arn"),
+				),
+			},
+			// Step 4: Switch back to S3 method to test memory of previous settings
+			{
+				Config: testAccCloudAwsAccountConfig_s3LogIngestionRequired(accountID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "account_id", accountID),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_method", "s3"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_s3_bucket_name", "test-cloudtrail-logs-bucket"),
+					resource.TestCheckResourceAttr(resourceName, "realtime_visibility.log_ingestion_sns_topic_arn", "arn:aws:sns:us-east-1:123456789012:cloudtrail-notifications"),
+				),
 			},
 		},
 	})
