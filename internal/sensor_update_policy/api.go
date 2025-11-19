2cd8e989c075c7451567cd77a59cb9f6fb28f6ba
Ryan Payne <ryan.payne@crowdstrike.com>
2025-11-19 10:49:55 -0600

    remove unneccesary apiScopes

--- internal/sensor_update_policy/api.go
+++ internal/sensor_update_policy/api.go
@@ -2,14 +2,6 @@ package sensorupdatepolicy
 
 import "github.com/crowdstrike/terraform-provider-crowdstrike/internal/scopes"
 
-var apiScopes = []scopes.Scope{
-	{
-		Name:  "sensor-update-policies",
-		Read:  true,
-		Write: true,
-	},
-}
-
 var dataSourceApiScopes = []scopes.Scope{
 	{
 		Name:  "sensor-update-policies",
