ce581880d73ca9d839e3c840da85064c12183487
ffalor <35144141+ffalor@users.noreply.github.com>
2025-11-24 15:53:37 -0600

    simplify import examples for Google Cloud registration

--- examples/resources/crowdstrike_cloud_google_registration/import.sh
+++ examples/resources/crowdstrike_cloud_google_registration/import.sh
@@ -1,5 +1,2 @@
 # Google Cloud Project Registration can be imported by specifying the registration ID
 terraform import crowdstrike_cloud_google_registration.example <registration_id>
-
-# Example with actual registration ID
-terraform import crowdstrike_cloud_google_registration.example 7fb858a949034a0cbca175f660f1e769
