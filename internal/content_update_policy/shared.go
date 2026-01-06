957fec3664b967aa59dfb5266900116de0a71a6a
ffalor <35144141+ffalor@users.noreply.github.com>
2026-01-06 16:23:53 -0600

    add content_update_policy_attachment resource

--- internal/content_update_policy/shared.go
+++ internal/content_update_policy/shared.go
@@ -3,13 +3,17 @@ package contentupdatepolicy
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/crowdstrike/gofalcon/falcon/client"
 	"github.com/crowdstrike/gofalcon/falcon/client/content_update_policies"
 	"github.com/crowdstrike/gofalcon/falcon/models"
+	hostgroups "github.com/crowdstrike/terraform-provider-crowdstrike/internal/host_groups"
+	"github.com/crowdstrike/terraform-provider-crowdstrike/internal/utils"
 	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
@@ -124,4 +128,148 @@ func updatePolicyEnabledState(
 	}
 
 	return err
+}
+
+// updateHostGroups adds or removes host groups from a content update policy.
+func updateHostGroups(
+	ctx context.Context,
+	client *client.CrowdStrikeAPISpecification,
+	action hostgroups.HostGroupAction,
+	hostGroupIDs []string,
+	policyID string,
+) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	if len(hostGroupIDs) == 0 {
+		return diags
+	}
+
+	var actionParams []*models.MsaspecActionParameter
+
+	actionMsg := "adding"
+	if action == hostgroups.RemoveHostGroup {
+		actionMsg = "removing"
+	}
+	name := "group_id"
+
+	for _, g := range hostGroupIDs {
+		gCopy := g
+		actionParam := &models.MsaspecActionParameter{
+			Name:  &name,
+			Value: &gCopy,
+		}
+
+		actionParams = append(actionParams, actionParam)
+	}
+
+	res, err := client.ContentUpdatePolicies.PerformContentUpdatePoliciesAction(
+		&content_update_policies.PerformContentUpdatePoliciesActionParams{
+			Context:    ctx,
+			ActionName: action.String(),
+			Body: &models.MsaEntityActionRequestV2{
+				ActionParameters: actionParams,
+				Ids:              []string{policyID},
+			},
+		},
+	)
+	if err != nil {
+		diags.AddError("Error updating content update policy host groups", fmt.Sprintf(
+			"Error %s host groups (%s) to content update policy (%s): %s",
+			actionMsg,
+			strings.Join(hostGroupIDs, ", "),
+			policyID,
+			err.Error(),
+		))
+
+		return diags
+	}
+
+	returnedHostGroups := make(map[string]bool)
+
+	if res != nil && res.Payload != nil {
+		for _, r := range res.Payload.Resources {
+			groups := r.Groups
+
+			for _, group := range groups {
+				returnedHostGroups[*group.ID] = true
+			}
+		}
+	}
+
+	if action == hostgroups.RemoveHostGroup {
+		for _, group := range hostGroupIDs {
+			_, ok := returnedHostGroups[group]
+			if ok {
+				diags.AddError(
+					"Error updating content update policy host groups",
+					fmt.Sprintf(
+						"Error %s host groups (%s) to content update policy (%s): %s",
+						actionMsg,
+						group,
+						policyID,
+						"Remove failed",
+					),
+				)
+			}
+		}
+	}
+
+	if action == hostgroups.AddHostGroup {
+		for _, group := range hostGroupIDs {
+			_, ok := returnedHostGroups[group]
+			if !ok {
+				diags.AddError(
+					"Error updating content update policy host groups",
+					fmt.Sprintf(
+						"Error %s host groups (%s) to content update policy (%s): %s",
+						actionMsg,
+						group,
+						policyID,
+						"Adding failed, ensure the host group is valid.",
+					),
+				)
+			}
+		}
+	}
+
+	if res != nil && res.Payload != nil {
+		for _, err := range res.Payload.Errors {
+			diags.AddError(
+				"Error updating content update policy host groups",
+				fmt.Sprintf(
+					"Error %s host groups (%s) to content update policy (%s): %s",
+					actionMsg,
+					err.ID,
+					policyID,
+					err.String(),
+				),
+			)
+		}
+	}
+
+	return diags
+}
+
+// syncHostGroups will sync the host groups from the resource model to the api.
+func syncHostGroups(
+	ctx context.Context,
+	client *client.CrowdStrikeAPISpecification,
+	planGroups, stateGroups types.Set,
+	id string,
+) diag.Diagnostics {
+	var diags diag.Diagnostics
+	groupsToAdd, groupsToRemove, diags := utils.SetIDsToModify(
+		ctx,
+		planGroups,
+		stateGroups,
+	)
+	diags.Append(diags...)
+	if diags.HasError() {
+		return diags
+	}
+
+	diags.Append(updateHostGroups(ctx, client, hostgroups.AddHostGroup, groupsToAdd, id)...)
+	diags.Append(updateHostGroups(ctx, client, hostgroups.RemoveHostGroup, groupsToRemove, id)...)
+
+	return diags
 }
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -260,6 +260,7 @@ func (p *CrowdStrikeProvider) Resources(ctx context.Context) []func() resource.R
 		contentupdatepolicy.NewContentPolicyResource,
 		contentupdatepolicy.NewDefaultContentUpdatePolicyResource,
 		contentupdatepolicy.NewContentUpdatePolicyPrecedenceResource,
+		contentupdatepolicy.NewContentUpdatePolicyAttachmentResource,
 		sensorvisibilityexclusion.NewSensorVisibilityExclusionResource,
 		itautomation.NewItAutomationTaskResource,
 		itautomation.NewItAutomationTaskGroupResource,
