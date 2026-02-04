d77612ff8e5c477358d09e046e10e70872a9a6d5
ffalor <35144141+ffalor@users.noreply.github.com>
2026-02-04 15:49:32 -0600

    Revert "refactor: rename data_protection_content_pattern to dataprotection_content_pattern"
    This reverts commit 58f3689ac76175012f378b327dabd2b366a4b784.

--- examples/resources/crowdstrike_data_protection_content_pattern/resource.tf
+++ examples/resources/crowdstrike_data_protection_content_pattern/resource.tf
@@ -1,4 +1,4 @@
-resource "crowdstrike_dataprotection_content_pattern" "api_key" {
+resource "crowdstrike_data_protection_content_pattern" "api_key" {
   name                = "API Key Pattern"
   description         = "Internal API Key pattern"
   regex               = "api[_-]?key[_-]?[a-zA-Z0-9]{32}"
--- examples/resources/crowdstrike_dataprotection_content_pattern/import.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Import an existing data protection content pattern by ID
-terraform import crowdstrike_dataprotection_content_pattern.example <pattern-id>
