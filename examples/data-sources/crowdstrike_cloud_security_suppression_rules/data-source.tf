1271f9fa90f31d4124e02d52c9252f5d9a3d11bf
Greg Pontejos <242696964+gpontejos-cs@users.noreply.github.com>
2026-02-05 18:50:54 -0600

    Update docs and gofalcon version

--- examples/data-sources/crowdstrike_cloud_security_suppression_rules/data-source.tf
+++ examples/data-sources/crowdstrike_cloud_security_suppression_rules/data-source.tf
@@ -11,33 +11,7 @@ provider "crowdstrike" {
 }
 
 # Get all suppression rules
-data "crowdstrike_cloud_security_suppression_rules" "all" {
-}
-
-# Filter by suppression rule type
-data "crowdstrike_cloud_security_suppression_rules" "by_type" {
-  type = "IOM"
-}
-
-# Filter by suppression reason
-data "crowdstrike_cloud_security_suppression_rules" "by_reason" {
-  reason = "false-positive"
-}
-
-# Filter by name with wildcard pattern
-data "crowdstrike_cloud_security_suppression_rules" "by_name" {
-  name = "Production*"
-}
-
-# Filter by description
-data "crowdstrike_cloud_security_suppression_rules" "by_description" {
-  description = "*load balancer*"
-}
-
-# Filter by comment text
-data "crowdstrike_cloud_security_suppression_rules" "by_comment" {
-  comment = "*approved by security team*"
-}
+data "crowdstrike_cloud_security_suppression_rules" "all" {}
 
 # Use FQL for advanced filtering
 data "crowdstrike_cloud_security_suppression_rules" "fql_advanced" {
