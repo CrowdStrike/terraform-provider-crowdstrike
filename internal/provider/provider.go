0837634b01cf802b0ea97163f69094c33077b3d1
Frank Falor <35144141+ffalor@users.noreply.github.com>
2026-02-06 15:06:54 -0600

    feat: add response_policy_precedence resource (#267)
    Add new resource for managing response policy precedence order by platform.
    Supports both strict and dynamic enforcement modes.

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -20,6 +20,7 @@ import (
 	hostgroups "github.com/crowdstrike/terraform-provider-crowdstrike/internal/host_groups"
 	itautomation "github.com/crowdstrike/terraform-provider-crowdstrike/internal/it_automation"
 	preventionpolicy "github.com/crowdstrike/terraform-provider-crowdstrike/internal/prevention_policy"
+	responsepolicy "github.com/crowdstrike/terraform-provider-crowdstrike/internal/response_policy"
 	sensorupdatepolicy "github.com/crowdstrike/terraform-provider-crowdstrike/internal/sensor_update_policy"
 	sensorvisibilityexclusion "github.com/crowdstrike/terraform-provider-crowdstrike/internal/sensor_visibility_exclusion"
 	"github.com/crowdstrike/terraform-provider-crowdstrike/internal/testconfig"
@@ -305,6 +306,7 @@ func (p *CrowdStrikeProvider) Resources(ctx context.Context) []func() resource.R
 		cloudgroup.NewCloudGroupResource,
 		cloudsecurity.NewCloudSecuritySuppressionRuleResource,
 		dataprotection.NewDataProtectionContentPatternResource,
+		responsepolicy.NewResponsePolicyPrecedenceResource,
 	}
 }
 
