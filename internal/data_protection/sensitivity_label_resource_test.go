ab3180987c882cabf4a4922044704baee115e633
GrantBirki <birki@openai.com>
2026-02-28 11:40:41 -0800

    test(data-protection): add standard sensitivity label coverage

--- internal/data_protection/sensitivity_label_resource_test.go
+++ internal/data_protection/sensitivity_label_resource_test.go
@@ -90,6 +90,44 @@ func TestAccDataProtectionSensitivityLabelResource_Basic(t *testing.T) {
 	})
 }
 
+func TestAccDataProtectionSensitivityLabelResource_Standard(t *testing.T) {
+	labelProvider := os.Getenv(testAccSensitivityLabelProviderEnvName)
+	if labelProvider == "" {
+		t.Skip("Skipping test that requires a data protection label provider. Set TF_ACC_DATA_PROTECTION_LABEL_PROVIDER to run this test.")
+	}
+
+	rName := acctest.RandomResourceName()
+	resourceName := "crowdstrike_data_protection_sensitivity_label.standard"
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataProtectionSensitivityLabelStandardConfig(rName, labelProvider),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttrSet(resourceName, "cid"),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "display_name", rName),
+					resource.TestCheckResourceAttr(resourceName, "label_provider", labelProvider),
+					resource.TestCheckResourceAttr(resourceName, "synced", "false"),
+					resource.TestCheckNoResourceAttr(resourceName, "external_id"),
+					resource.TestCheckNoResourceAttr(resourceName, "plugins_configuration_id"),
+					resource.TestCheckNoResourceAttr(resourceName, "co_authoring"),
+					resource.TestCheckResourceAttrSet(resourceName, "created_at"),
+					resource.TestCheckResourceAttrSet(resourceName, "last_updated"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
 func testAccDataProtectionSensitivityLabelConfig(
 	name string,
 	displayName string,
@@ -110,4 +148,17 @@ resource "crowdstrike_data_protection_sensitivity_label" "test" {
   synced                   = %[7]t
 }
 `, name, displayName, externalID, labelProvider, pluginConfigID, coAuthoring, synced)
+}
+
+func testAccDataProtectionSensitivityLabelStandardConfig(
+	name string,
+	labelProvider string,
+) string {
+	return acctest.ProviderConfig + fmt.Sprintf(`
+resource "crowdstrike_data_protection_sensitivity_label" "standard" {
+  name           = %[1]q
+  label_provider = %[2]q
+  synced         = false
+}
+`, name, labelProvider)
 }
