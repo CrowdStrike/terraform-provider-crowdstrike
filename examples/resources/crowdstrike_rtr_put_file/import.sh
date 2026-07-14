54116deb2d223f086105faec5fbda99ba2c1de36
João Mesquita <151409184+jfgmesquita@users.noreply.github.com>
2026-07-14 17:50:21 +0100

    fix(docs): update import syntax to use "<id>_<suffix>" for consistency

--- examples/resources/crowdstrike_rtr_put_file/import.sh
+++ examples/resources/crowdstrike_rtr_put_file/import.sh
@@ -1,2 +1,2 @@
-# Rtr Put File can be imported by specifying the compound ID "<file_id>_<cid>".
+# Rtr Put File can be imported by specifying the compound ID "<file_id>_<suffix>".
 terraform import crowdstrike_rtr_put_file.example 7fb858a949034a0cbca175f660f1e769_1234567890abcdef
